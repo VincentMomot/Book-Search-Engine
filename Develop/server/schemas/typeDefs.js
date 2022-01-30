06fdb06b9c9d9b45e831d49e92f07ceec7c26a2e
Vince Momot <vincemomot@gmail.com>
2022-01-30 08:11:27 -0500

    rewriting some schemas copied from another activity

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    bookCount: String
+    savedBooks: [Book]
+   }
+
+   type Book {
+    authors: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+   }
+ 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(name: String!): User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
